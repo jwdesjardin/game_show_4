2eb2ed574823d53732970c4054dc0a7d5af41d70
jwdesjardin <63127698+jwdesjardin@users.noreply.github.com>
2020-10-30 20:18:10 -0700

    updated css and reset bug

--- js/Game.js
+++ js/Game.js
@@ -2,15 +2,15 @@
  * Project 4 - OOP Game App
  * Game.js */
 
-
  class Game {
+     
      constructor(){
          this.missed = 0;
          this.phrases = [   new Phrase('coding'), 
                             new Phrase('programming'), 
                             new Phrase('javascript'), 
                             new Phrase('treehouse'), 
-                            new Phrase('banana rama')];
+                            new Phrase('object')];
          this.activePhrase = null;
      }
 
@@ -19,15 +19,12 @@
         document.querySelector('#overlay').style.display = 'none';
         this.activePhrase = this.getRandomPhrase();
         this.activePhrase.addPhraseToDisplay();
-        console.log(this.activePhrase);
     }
     getRandomPhrase(){
         const randomInt = Math.floor(Math.random() * this.phrases.length);
         return this.phrases[randomInt];
     }
     handleInteraction = (target) => {
-        console.log(this.activePhrase);
-        console.log(target);
         target.setAttribute('disabled', true);
         const guess = target.textContent;
         if(this.activePhrase.phrase.includes(guess)){
@@ -42,10 +39,11 @@
         }
     }
     removeLife(){
-        const liveHeart = document.querySelector('#scoreboard li img[src="images/liveHeart.png"]');
-        console.log(liveHeart);
-        liveHeart.setAttribute('src', 'images/lostHeart.png');
+        //selects the first heart image and sets to miss image
+        const liveHeart = document.querySelector('#scoreboard li img[src="images/heart.png"]');
+        liveHeart.setAttribute('src', 'images/miss.png');
 
+        // inc missed and check for win
         this.missed++;
         if (this.missed >= 5){
             this.gameOver();
@@ -54,6 +52,7 @@
     }
 
     checkForWin(){
+        //loop through phrase lis if any are hideen return false
         const lis = document.querySelectorAll('#phrase ul li');
         for(let i = 0; i < lis.length; i++){
             if (lis[i].className.includes('hide')){
@@ -65,14 +64,36 @@
     
     gameOver(){
         const overlay = document.querySelector('#overlay');
-        overlay.style.display = 'block';
+        overlay.style.display = 'flex';
         const message = overlay.querySelector('#game-over-message');
+        const emojiDiv = overlay.querySelector('#game-over-emoji');
+        const emoji = emojiDiv.firstElementChild;
+        const button = emoji.parentElement.nextElementSibling;
+
+
+
         if (this.missed >= 5){
+            //change text and background color
             message.textContent = 'You Lost';
+            button.textContent = 'Play Again?'
             overlay.className = 'lose';
+            //display forwning emoji
+            emoji.setAttribute('src', 'images/frown1.png');
+            emojiDiv.style.display = 'block';
+            if (emojiDiv.previousElementSibling.classList.contains('fas')){
+                emojiDiv.previousElementSibling.remove();
+            }
         } else {
+            //change text and background color
             message.textContent = 'You Won!';
+            button.textContent = 'Play Again?'
             overlay.className = 'win';
+            //display smiling emoji
+            emoji.setAttribute('src', 'images/smile.png');
+            emojiDiv.style.display = 'block';
+            if (emojiDiv.previousElementSibling.classList.contains('fas')){
+                emojiDiv.previousElementSibling.remove();
+            }
         }
         this.resetGame();
     }
@@ -81,6 +102,10 @@
         const phraseUl = document.querySelector('#phrase').firstElementChild;
         phraseUl.innerHTML = '';
      
+        //reset properties
+        this.missed = 0;
+        this.activePhrase = null;
+
         //reset keys
         const qwerty = document.querySelector('#qwerty').children;
         for (let i = 0; i < qwerty.length; i++){
@@ -93,9 +118,8 @@
         
         //reset hearts
         const hearts = document.querySelectorAll('#scoreboard li img');
-        console.log(hearts);
         hearts.forEach(li => {
-            li.setAttribute('src', 'images/liveHeart.png')
+            li.setAttribute('src', 'images/heart.png')
         })
     }
  }
--- js/Phrase.js
+++ js/Phrase.js
@@ -4,39 +4,26 @@
 
  class Phrase {
 
+    // this.phrase is a all lowercase string
     constructor(phrase){
         this.phrase = phrase.toLowerCase();
     }
 
     addPhraseToDisplay() {
-        const html = `
-        <div id="phrase" class="section">
-            <ul>
-                <li class="hide letter h">h</li>
-                <li class="hide letter o">o</li>
-                <li class="hide letter w">w</li>
-                <li class="space"> </li>
-                <li class="hide letter a">a</li>
-                <li class="hide letter r">r</li>
-                <li class="hide letter e">e</li>
-                <li class="space"> </li>
-                <li class="hide letter y">y</li>
-                <li class="hide letter o">o</li>
-                <li class="hide letter u">u</li>
-            </ul>
-        </div>
-        `
         const phrase = this.phrase;
         const phraseDiv = document.querySelector('#phrase');
         const ul = phraseDiv.firstElementChild;
 
+        //loop through the this.phrase string
         for (let i = 0; i< this.phrase.length; i++){
+            // if its a word character, create and append char li
             if (phrase[i].search(/\w/) != -1){
                 const li = document.createElement('li');
                 li.className += (`hide letter ${phrase[i]}`);
-                const textNode = document.createTextNode(`${phrase[i]}`);
+                const textNode = document.createTextNode(`.`);
                 li.appendChild(textNode);
                 ul.appendChild(li);
+            //if its a space create and append space li
             } else if ((phrase[i].search(/\s/) != -1)){
                 const li = document.createElement('li');
                 li.classList.add(`space`);
@@ -47,11 +34,13 @@
         }
     }
  
+    
     checkLetter(letter) {
         return this.phrase.includes(letter);
 
     }
  
+    //selects elements that match letter and shows the letter
     showMatchedLetter(letter) {
         const phraseDiv = document.querySelector('#phrase');
         const ul = phraseDiv.firstElementChild;
@@ -59,6 +48,7 @@
         domElements.forEach(li => {
             li.classList.remove('hide');
             li.classList.add('show');
+            li.textContent = letter;
         })
     }
  }
